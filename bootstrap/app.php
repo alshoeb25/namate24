006b996a397e51b0c082abfce82ae1ace6bf6f3f
Shoeb Alam <ashoeb20@gmail.com>
2026-01-09 20:04:49 +0530

    Implement Livewire

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Livewire\Livewire;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,6 +12,12 @@ return Application::configure(basePath: dirname(__DIR__))
         api: __DIR__.'/../routes/api.php',
         channels: __DIR__.'/../routes/channels.php',
         health: '/up',
+        then: function () {
+            // Ensure Livewire routes are registered with web middleware
+            Livewire::setUpdateRoute(function ($handle) {
+                return Route::post('/livewire/update', $handle)->middleware(['web']);
+            });
+        },
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
